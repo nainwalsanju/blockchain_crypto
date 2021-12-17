a2b2018237f454695eba8366b009803e16d8a2e5
Sanjay Nainwal <nainwalsanju@github.com>
2021-12-17 21:49:13 +0530

    generatin the signature

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -5,6 +5,9 @@ import Crypto.Random
 from Crypto.PublicKey import RSA
 import binascii
 from collections import OrderedDict
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+
 
 class Transaction:
 
@@ -21,6 +24,13 @@ class Transaction:
             "recipient_public_key": self.recipient_public_key,
             "amount": self.amount,
         })
+
+    def sign_transaction(self):
+        private_key=RSA.importKey(binascii.unhexlify(self.sender_private_key))
+        signer = PKCS1_v1_5.new(private_key)
+        hash = SHA.new(str(self.to_dict()).encode("utf8"))
+        return binascii.hexlify(signer.sign(hash)).decode('ascii')
+
 
 app = Flask(__name__)
 
@@ -42,7 +52,7 @@ def generate_transaction():
     transaction = Transaction(sender_public_key, sender_private_key, recipient_public_key, amount)
     response = {
         'transaction': transaction.to_dict(),
-        'signature': 'blah'
+        'signature': transaction.sign_transaction()
     }
 
     return jsonify(response),200
