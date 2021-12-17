bdec28d07488548f742850c54b9342039198c87c
Sanjay Nainwal <nainwalsanju@github.com>
2021-12-17 20:48:25 +0530

    genrate private and public key

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -1,4 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+import Crypto
+import Crypto.Random
+from Crypto.PublicKey import RSA
+import binascii
 
 class Transaction:
 
@@ -24,7 +28,14 @@ def view_transactions():
 
 @app.route('/wallet/new')
 def new_wallet():
-    return ''
+    random_gen = Crypto.Random.new().read
+    private_key = RSA.generate(1024, random_gen)
+    public_key = private_key.public_key()
+    response = {
+        "private_key":binascii.hexlify( private_key.export_key(format("DER"))).decode('ascii'),
+        "public_key": binascii.hexlify( public_key.export_key(format("DER"))).decode('ascii'),
+    }
+    return jsonify(response),200
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
